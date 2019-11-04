545381df79579bf1190c8e635af2d4eae66915f6
e-victoria <evictoria@protonmail.com>
2019-11-04 14:47:40 +0100

    Add abstract Player

---
+++ src/main/java/Player.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public abstract class Player {
+
+    public abstract void getHand();
+
+    public abstract void addPoints();
+
+    public abstract void getPoints();
+
+    public abstract void getName();
+
+}
