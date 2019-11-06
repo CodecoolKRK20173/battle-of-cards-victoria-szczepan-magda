32518520b13795739cab23d496d24927b92cdc35
e-victoria <evictoria@protonmail.com>
2019-11-06 14:29:03 +0100

    Fix bug

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -17,11 +17,9 @@ public class Table {
         this.player1 = player1;
         this.player2 = player2;
 
-        player1LastCardIndex = player1TopCards.size() - 1;
-        player2lastCardIndex = player2TopCards.size() - 1;
-
         player1TopCards = new ArrayList<>();
         player2TopCards = new ArrayList<>();
+
         cardsOnTable = new LinkedHashMap<>();
     }
 
@@ -51,6 +49,8 @@ public class Table {
     }
 
     public Boolean getBattleResult(Player player, String parameter) {
+        player1LastCardIndex = player1TopCards.size() - 1;
+        player2lastCardIndex = player2TopCards.size() - 1;
         Boolean isResolved = null;
         if (player.equals(player1)) {
             isResolved = player1TopCards.get(player1LastCardIndex).compareCards(player2TopCards.get(player2lastCardIndex), parameter);
